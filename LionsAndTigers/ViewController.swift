4f02e6a47289521bc62e5a5245d1b181cad0eb39
Philip Pang <ohilib@gmail.com>
2015-04-18 17:10:28 +0800

    first commit

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,16 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    var myTigers:[Tiger] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var myTiger = Tiger()
+        myTiger.name = "tigger"
+        myTiger.age = 3
+        myTiger.breed = "Bengel"
+        myTiger.image = UIImage(named: "BengelTiger.jpg")
+        
+        println("My tiger name is \(myTiger.name), its' age is \(myTiger.age), its' breed is \(myTiger.breed), and its' image is \(myTiger.image)")
+    
+        myImageView.image = myTiger.image
+        nameLabel.text = myTiger.name
+        ageLabel.text = "\(myTiger.age)"
+        breedLabel.text = myTiger.breed
+        
+        var secondTiger = Tiger()
+        secondTiger.name = "Tigress"
+        secondTiger.age = 2
+        secondTiger.breed = "Indochinese Tiger"
+        secondTiger.image = UIImage(named: "IndochineseTiger.jpg")
+        
+        var thirdTiger = Tiger()
+        thirdTiger.name = "Jacob"
+        thirdTiger.age = 4
+        thirdTiger.breed = "Malaysan Tiger"
+        thirdTiger.image = UIImage(named: "MalayanTiger.jpg")
+        
+        
+        var forthTiger = Tiger()
+        forthTiger.name = "Spar"
+        forthTiger.age = 3
+        forthTiger.breed = "Siberian Tiger"
+        thirdTiger.image = UIImage(named: "SiberianTiger.jpg")
+    
+        
+        myTigers += [secondTiger, thirdTiger, forthTiger]
     }
-
+   
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBOutlet weak var nextBarButtonItemPressed: UIBarButtonItem!
 
 }
 
---
+++ Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Philip Pang on 4/4/15.
+//  Copyright (c) 2015 Philip Pang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+} 
